d9a2ea3ea8efd2db0c2ba6e5230b8a807edfc6e9
Anish2504 <anish.kanswal@gmail.com>
2020-02-21 09:46:05 +0100

    removed space between src and imagelink in index file

--- src/hello.jsx
+++ src/hello.jsx
@@ -5,7 +5,7 @@ const Hello = () => {
     <div className="ui main container">
       <h1 id="hello">Junior Developer</h1>
      
-      <img src= "https://thumbs.dreamstime.com/b/music-cogs-technology-background-conceptual-image-streaming-72736889.jpg"></img>
+      <img src="https://thumbs.dreamstime.com/b/music-cogs-technology-background-conceptual-image-streaming-72736889.jpg"></img>
       <h2>Coding is a journey not a destination</h2>
     </div>
   );
